8cd0940eef19b1790162e8ec2ab8d595f427c9ee
nemanjan00 <nemanjan00@gmail.com>
2022-07-04 21:06:55 +0200

    Prepare environment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 const Docker = require("dockerode");
+const image = require("./image");
 
-const docker = new Docker();
+const builder = {
+	_docker: undefined,
 
-const image = require("./image")(docker);
+	_init: () => {
+		builder._docker = new Docker();
+	},
 
-image.buildImage().then((data) => {
-	console.log(data);
-});
+	_prepareEnvironment: [
+		(docker) => {
+			console.log("Building image...");
+			return image(docker).buildImage();
+		}
+	],
+
+	prepareEnvironment: () => {
+		console.log("Preparing environment...");
+
+		return builder._prepareEnvironment.reduce((prev, cur) => prev.then(() => cur(builder._docker)), Promise.resolve()).then(() => {
+			console.log("Prepared environment... Ready to accept commands...");
+		});
+	}
+};
+
+builder._init();
+
+builder.prepareEnvironment().then(() => {
+}).catch(console.error);
